c4a81837e9cebe2329ea383301298fcedc992221
Cherebrus <74614035+Cherebrus@users.noreply.github.com>
2020-11-19 16:41:01 +0200

    ArtyomPush

---
+++ GitHw/Assets/NewScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int i = 5;
+
+        if (i == 4)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
